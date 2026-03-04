94dba2f03cc4b0106e95cd24ee49b1ef43bc3d53
Ondresss <ondra.mic2@gmail.com>
2026-03-04 11:09:55 -0600

    Finished test

--- 32bitStack_exercise2/main.c
+++ 32bitStack_exercise2/main.c
@@ -7,19 +7,25 @@ int longest_space( char *str );
 int kolik_mocnin( int *pole, int N, int K );
 int binstr2int( char *binstr );
 
+
+char* poz_nejdelsi_mezery( char *str );
+int biggest_negative(int* arr,int len);
+int biggest_remainder_2(int* arr,int len,int K);
+int encode_chars(char* str);
 char str[] = "Pro gram   ovani v asse bleru . ";
 int main(void) {
-    int nums[] =  { -10, 10, -20, 20, -30, 30, -40, 40, -50, 50 };
-    int numsK[] =  { 2,16,3 };
-    int lowestPositive = findLowestPositiveNumber(nums,10);
-    printf("lowestPositive = %d\n",lowestPositive);
-    int longestSpace = longest_space(str);
-    printf("longestSpace = %d\n",longestSpace);
 
-    int nKBits = kolik_mocnin(numsK,3,1);
-    printf("K Bits: %d\n",nKBits);
-    char binCode[] = "1010101001010101";
-    int number = binstr2int(binCode );
-    printf("%x\n",number);
+    int arr[] = { -1, 10, -20, 20, -30, 30, -40, 40, -50, 50 };
+    int arrRemainder[] = {2,4,16};
+    int biggestNegative = biggest_negative(arr,10);
+    printf("%d\n",biggestNegative);
+    char* pos = poz_nejdelsi_mezery(str);
+    printf("%s\n",pos);
+
+    int numRemainder = biggest_remainder_2(arrRemainder,3,1);
+    printf("%d\n",numRemainder);
+    char strToEncode[] = "SMALL blacK CAT";
+    int encodedNum = encode_chars(strToEncode);
+    printf("%x\n",encodedNum);
     exit(EXIT_SUCCESS);
 }
--- 32bitStack_exercise2/start.asm
+++ 32bitStack_exercise2/start.asm
@@ -5,7 +5,175 @@ section .data
 
 
 section .text
-    global findLowestPositiveNumber,longest_space,num_of_powers,binstr2int,kolik_mocnin
+    global findLowestPositiveNumber,longest_space,num_of_powers,binstr2int,kolik_mocnin,poz_nejdelsi_mezery,biggest_negative,biggest_remainder_2,encode_chars
+
+
+encode_chars:
+    enter 0,0
+    push esi
+    xor eax,eax
+    xor ecx,ecx
+    xor esi,esi
+    mov edx,dword [ebp + 8]
+.loop:
+    cmp byte [edx + esi],0
+    je .end
+    movzx ecx,byte [edx + esi]
+    cmp ecx,byte 'A'
+    jl .invalidChar
+    cmp ecx,byte 'Z'
+    jg .invalidChar
+    shl eax,1
+    or eax,1
+    inc esi
+    jmp .loop
+.invalidChar:
+    cmp ecx,byte 'a'
+    jl .nothing
+    cmp ecx,byte 'z'
+    jg .nothing
+    shl eax,1
+    or eax,0
+.nothing:
+   inc esi
+   jmp .loop
+
+
+.end:
+    pop esi
+    leave
+    ret
+
+
+
+biggest_remainder_2:
+    enter 0,0
+    push ebx
+    push esi
+    push edi
+    xor ecx,ecx
+    xor eax,eax
+    mov edx,[ebp + 8]
+    xor ebx,ebx
+    xor esi,esi
+    xor edi,edi
+.loop:
+    xor ebx,ebx
+    cmp ecx,dword [ebp + 12]
+    jae .end
+    mov esi,dword [edx + ecx * 4]
+.checkKBits:
+    test esi,1
+    jne .endCheck
+    inc ebx
+    shr esi,1
+    jmp .checkKBits
+.endCheck:
+    cmp ebx,dword [ebp + 16]
+    jl .dontUpdateCount
+    cmp edi,ebx
+    jle .swapCurrentBiggest
+    jmp .dontUpdateCount
+.swapCurrentBiggest:
+    mov edi,ebx
+    mov eax,dword [edx + ecx * 4]
+.dontUpdateCount:
+    inc ecx
+    jmp .loop
+.end:
+    pop edi
+    pop esi
+    pop ebx
+    leave
+    ret
+
+
+biggest_negative:
+    enter 0,0
+    xor ecx,ecx
+    xor eax,eax
+    mov edx,[ebp + 8]
+.firstPositive:
+    cmp ecx, [ebp + 12]
+    jae .end
+    cmp dword [edx + ecx * 4],0
+    jl  .foundPos
+    inc ecx
+    jmp .firstPositive
+.foundPos:
+    mov eax,[edx + ecx * 4]
+    xor ecx,ecx
+.loop:
+    cmp ecx, [ebp + 12]
+    jae .end
+    cmp dword [edx + ecx * 4],0
+    jge .skipPoz
+    cmp eax, [edx + ecx * 4]
+    jle .swap
+    inc ecx
+    jmp .loop
+.swap:
+    mov eax,[edx + ecx * 4]
+    inc ecx
+    jmp .loop
+.skipPoz:
+    inc ecx
+    jmp .loop
+.end:
+    leave
+    ret
+
+
+
+
+
+
+poz_nejdelsi_mezery:
+    enter 0,0
+    push ebx
+    push edi
+    xor ebx,ebx
+    xor ecx,ecx
+    xor eax,eax
+    mov edx,[ebp + 8]
+.loop:
+    cmp byte [edx + ecx],0
+    je .end
+    cmp byte [edx + ecx], ' '
+    je .foundSpace
+    cmp eax,ebx
+    jae .skipCount
+    mov eax,ebx
+    xor ebx,ebx
+    mov edi,ecx
+.skipCount:
+    inc ecx
+    xor ebx,ebx
+    jmp .loop
+.foundSpace:
+    inc ebx
+    inc ecx
+    jmp .loop
+.end:
+    cmp eax,ebx
+    jae .skip
+    mov eax,ebx
+.skip:
+    sub edi,eax
+    lea eax,[edx + edi]
+    pop edi
+    pop ebx
+    leave
+    ret
+
+
+
+
+
+
+
+
+
 
 kolik_mocnin:
     enter 0,0
