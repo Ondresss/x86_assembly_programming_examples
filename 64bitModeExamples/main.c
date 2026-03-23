d3c1a79d9d43244f8c623ea0d41d9ca222400c5a
Ondresss <ondra.mic2@gmail.com>
2026-03-23 20:39:24 +0100

    64bit mode

--- 64bitModeExamples/main.c
+++ 64bitModeExamples/main.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+void str_up_low( char *str, int up_down );
 void sums_of_bytes( long *pole, int N );
+void long2str( long num, char *cislo );
 int main(void) {
     long nums[] = {0x0102010201020102};
     sums_of_bytes(nums,1);
@@ -10,5 +13,14 @@ int main(void) {
         printf("%ld ", nums[i]);
     }
     printf("\n");
+    long num = 0x6162636465666768;
+    char cislo[9] = {0};
+    long2str(num,cislo);
+    printf("%s\n",cislo);
+
+    char str[128] = "ahoj";
+    str_up_low(str,1);
+    printf("%s\n",str);
+
     exit(EXIT_SUCCESS);
 }
--- 64bitModeExamples/start.asm
+++ 64bitModeExamples/start.asm
@@ -2,9 +2,58 @@ bits 64
 
 
 section .data
-    global sums_of_bytes
+    global sums_of_bytes,long2str,str_up_low
 
 section .text
+
+str_up_low:
+    enter 0,0
+    xor rcx,rcx
+    xor rax,rax
+    xor rbx,rbx
+    xor rdx,rdx
+    cmp rsi,0
+    je .setupDown
+    mov al,'a'
+    mov bl,'z'
+    mov dl,-32
+    jmp .loop
+.setupDown:
+    mov al,'A'
+    mov bl,'Z'
+    mov dl,32
+.loop:
+    cmp byte [rdi + rcx],0
+    je .end
+    cmp byte [rdi + rcx],al
+    jl .skip
+    cmp byte [rdi + rcx],bl
+    jg .skip
+    add byte [rdi + rcx],dl
+.skip:
+    inc rcx
+    jmp .loop
+.end:
+    leave
+    ret
+
+long2str:
+    enter 0,0
+    xor rcx,rcx
+    xor r8,r8
+    mov r8,7
+.loop:
+    cmp rcx,8
+    jae .end
+    mov byte [rsi + r8],dil
+    shr rdi,8
+    inc rcx
+    dec r8
+    jmp .loop
+.end:
+    leave
+    ret
+
 
 sums_of_bytes:
     enter 0,0
