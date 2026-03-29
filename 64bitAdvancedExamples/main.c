a83993b039284afd6cc4077a1559a7932607c3d6
Ondresss <ondra.mic2@gmail.com>
2026-03-28 19:15:58 -0500

    64 bit mode Advanced

--- 64bitAdvancedExamples/main.c
+++ 64bitAdvancedExamples/main.c
@@ -3,11 +3,19 @@
 #include <string.h>
 
 void bubble( int *pole, int N );
+void bez_opakovani( char *str );
 int main(void) {
     int sortArr[128] = {15,4,10,1,12,3};
     bubble(sortArr,6);
     for (int i = 0; i < 6; ++i) {
         printf("%d ",sortArr[i]);
+    }
+    printf("\n");
+
+    char characters[128] = "abcccde";
+    bez_opakovani(characters);
+    for (int i = 0; characters[i] != '\0'; ++i) {
+        printf("%c ",characters[i]);
     }
     printf("\n");
     exit(EXIT_SUCCESS);
--- 64bitAdvancedExamples/start.asm
+++ 64bitAdvancedExamples/start.asm
@@ -4,7 +4,7 @@ bits 64
 section .data
 
 section .text
-      global bubble
+      global bubble,bez_opakovani
 
 
 bubble:
@@ -38,3 +38,40 @@ bubble:
 .end:
     leave
     ret
+
+
+bez_opakovani:
+    enter 256,0
+    push rbx
+    push rdi
+    lea rdi,[ rbp - 256 ]
+    xor rax,rax
+    mov rcx,256
+    rep stosb
+    mov rdx,rdi
+    pop rdi
+    xor rcx,rcx
+    mov rbx,rdi
+.loop:
+    mov r8b,byte [ rdi + rcx ]
+    cmp r8d,0
+    jz .end
+    inc byte [ rdx + r8 ]
+    cmp byte [ rdx + r8 ],2
+    jge .skip
+    mov byte [ rbx ],r8b
+    inc rbx
+    inc rcx
+    jmp .loop
+.skip:
+    inc rcx
+    jmp .loop
+.end:
+    mov [rbx],0
+    pop rbx
+    leave
+    ret
+
+
+
+
