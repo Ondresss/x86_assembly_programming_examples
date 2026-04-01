0b27445661d00d01f81a89d52bb9c0474e0c0536
Ondresss <ondra.mic2@gmail.com>
2026-04-01 11:18:23 -0500

    64 bit mode Advanced

--- 64bitAdvancedExamples/main.c
+++ 64bitAdvancedExamples/main.c
@@ -4,19 +4,26 @@
 
 void bubble( int *pole, int N );
 void bez_opakovani( char *str );
+int median( int *pole, int N );
+int bSearch(int* pole,int l,int r,int x);
+void bubbleChars( char* str, int len );
+void bez_znaku( char *str, char *co_nechci );
 int main(void) {
-    int sortArr[128] = {15,4,10,1,12,3};
-    bubble(sortArr,6);
-    for (int i = 0; i < 6; ++i) {
-        printf("%d ",sortArr[i]);
-    }
-    printf("\n");
 
-    char characters[128] = "abcccde";
-    bez_opakovani(characters);
-    for (int i = 0; characters[i] != '\0'; ++i) {
-        printf("%c ",characters[i]);
+    char charsToSort[128] = "dbdz";
+    bubbleChars(charsToSort,6);
+    for (int i = 0; i < 6; i++) {
+        printf("%c ",charsToSort[i]);
     }
     printf("\n");
+    char charsRem[128] = "aabc";
+    bez_znaku(charsRem,"aa");
+    for (int i =0; charsRem[i] != '\0'; i++) {
+        printf("%c ",charsRem[i]);
+    }
+    printf("\n");
+    int nums[128]  = {2,5,1,4};
+    int pos = bSearch(nums,0,4,1);
+    printf("pos = %d\n",pos);
     exit(EXIT_SUCCESS);
 }
--- 64bitAdvancedExamples/start.asm
+++ 64bitAdvancedExamples/start.asm
@@ -2,10 +2,9 @@ bits 64
 
 
 section .data
-
+      firstLoop dd 0
 section .text
-      global bubble,bez_opakovani
-
+      global bubble,bez_opakovani,median,bSearch,removeDuplicates,bez_znaku,bubbleChars
 
 bubble:
     enter 0,0
@@ -48,7 +47,7 @@ bez_opakovani:
     xor rax,rax
     mov rcx,256
     rep stosb
-    mov rdx,rdi
+    lea rdx,[ rbp - 256 ]
     pop rdi
     xor rcx,rcx
     mov rbx,rdi
@@ -72,6 +71,169 @@ bez_opakovani:
     leave
     ret
 
+median:
+    enter 0,0
+    mov rax,rsi
+    shl rax,2
+    sub rsp,rax
+    mov rcx,rsi
+    mov r12,rsi
+    mov rsi,rdi
+    mov rdi,rsp
+    rep movsd
+    mov rdi,rsp
+    mov rsi,r12
+    call bubble
+    xor rdx,rdx
+    mov rdi,rsp
+    mov eax,r12d
+    mov r11d,2
+    idiv r11d
+    cmp edx,0
+    jnz .odd
+    mov r13d,dword [rdi + rax * 4]
+    mov r14d,dword [rdi + rax * 4 -4]
+    cdq
+    add r14d,r13d
+    mov eax,r14d
+    idiv r11d
+    jmp .end
+.odd:
+    mov eax,dword [ rdi + rax * 4 ]
+.end:
+    leave
+    ret
 
+
+
+bubbleChars:
+    enter 0,0
+    cmp dword [firstLoop],0
+    jnz .con
+    mov r13,rdi
+    mov r14,rsi
+.con:
+    push rdi
+    push rsi
+    cmp rsi,0
+    jle .end
+    mov rcx,rsi
+    dec rcx
+.loop:
+    cmp rcx,0
+    jle .rec
+    mov r8b,byte [rdi + rcx]
+    mov r9b,byte [rdi + rcx-1]
+    cmp r8b,r9b
+    jle .swap
+    jmp .continue
+.swap:
+    cmp [firstLoop],0
+    jne .first
+    jmp .currentSwap
+.first:
+    cmp r8b,r9b
+    je .areEqual
+    jmp .currentSwap
+.areEqual:
+    mov r15b,byte [r13 + r14 - 1]
+    mov byte [rdi],r15b
+    jmp .continue
+.currentSwap:
+    mov byte [rdi + rcx],r9b
+    mov byte [rdi + rcx-1],r8b
+.continue:
+    dec rcx
+    jmp .loop
+.rec:
+    inc [firstLoop]
+    add rdi,1
+    dec rsi
+    call bubbleChars
+.end:
+
+    leave
+    ret
+
+
+
+
+
+
+bSearch:
+    enter 0,0
+    cmp rsi,rdx
+    jg .notFound
+    push rdx
+    mov r12,2
+    mov rax,rdx
+    sub rax,rsi
+    add rax,rsi
+    xor rdx,rdx
+    idiv r12
+    pop rdx
+    mov r12d,dword [ rdi + rax * 4 ]
+    cmp r12d,ecx
+    jl .greater
+    jg .lower
+    jmp .end
+.greater:
+    mov rsi,rax
+    inc rsi
+    call bSearch
+    jmp .end
+.lower:
+    mov rdx,rax
+    dec rdx
+    call bSearch
+    jmp .end
+.notFound:
+    mov rax,-1
+.end:
+    leave
+    ret
+
+
+bez_znaku:
+    enter 256,0
+    push rbx
+    push rdi
+    lea rdi,[ rbp - 256 ]
+    xor rax,rax
+    mov rcx,256
+    rep stosb
+    lea rdx,[ rbp - 256 ]
+    pop rdi
+    xor rcx,rcx
+    mov rbx,rdi
+    xor r8,r8
+.init:
+    mov r8b,byte [rsi + rcx]
+    cmp r8b,0
+    jz .doneInit
+    inc byte [rdx + r8]
+    inc rcx
+    jmp .init
+.doneInit:
+    xor rcx,rcx
+    xor r8,r8
+.loop:
+    mov r8b,byte [ rdi + rcx ]
+    cmp r8d,0
+    jz .end
+    cmp byte [ rdx + r8 ],1
+    jge .skip
+    mov byte [ rbx ],r8b
+    inc rbx
+    inc rcx
+    jmp .loop
+.skip:
+    inc rcx
+    jmp .loop
+.end:
+    mov [rbx],0
+    pop rbx
+    leave
+    ret
 
 
